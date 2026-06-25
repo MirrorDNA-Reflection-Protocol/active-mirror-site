88ef7af8178cd02463d3b336ea62d30b8932c83f
Paul <paul.mirroros@hotmail.com>
2026-06-25 18:54:30 +0530

    Route root to explicit app shell

--- public/app/assets/NotFound-CWRlp3mT.js
+++ public/app/assets/NotFound-CWRlp3mT.js
@@ -1,4 +1,4 @@
-import{c as i,j as e,L as t}from"./index-DIwhnqPL.js";import{A as a}from"./arrow-left-BHQDkn6r.js";/**
+import{c as i,j as e,L as t}from"./index-CrzpuwRj.js";import{A as a}from"./arrow-left-TdtV-wnI.js";/**
  * @license lucide-react v0.469.0 - ISC
  *
  * This source code is licensed under the ISC license.
