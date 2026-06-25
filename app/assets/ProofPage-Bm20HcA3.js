486c8e0449358ea45db3e2bed85d9a97d45f1ffe
MirrorDNA-Reflection-Protocol <MirrorDNA-Reflection-Protocol@users.noreply.github.com>
2026-06-25 12:44:23 +0000

    deploy: 99f66a020a5fc4d3d7886327e848cd7c03de3f29

--- app/assets/ProofPage-Bm20HcA3.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./index-DrtcRPN2.js";const t=()=>(o.useEffect(()=>{window.location.href="/proof/index.html"},[]),null);export{t as default};
---
+++ app/assets/ProofPage-oZVmNm4I.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-DXGJ1fsx.js";const t=()=>(o.useEffect(()=>{window.location.href="/proof/index.html"},[]),null);export{t as default};
