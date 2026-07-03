c1108d4ff5d49f751a40d07f264afab62db1f365
Paul <paul.mirroros@hotmail.com>
2026-07-03 18:43:58 +0530

    Publish simplified start setup

--- public/app/assets/Privacy-BlFvR8Kb.js
+++ public/app/assets/Privacy-BlFvR8Kb.js
@@ -1,4 +1,4 @@
-import{c as t,j as e,L as a,a as o,T as i}from"./index-DoIcKOYP.js";import{A as n}from"./arrow-left-C40NvMAh.js";import{S as c}from"./shield-BW9Um4uu.js";import{M as r}from"./mail-1IOgAqQp.js";/**
+import{c as t,j as e,L as a,a as o,T as i}from"./index-C1UUmAtm.js";import{A as n}from"./arrow-left-BDquwecT.js";import{S as c}from"./shield-mXHnPOc5.js";import{M as r}from"./mail-DJCDwIVd.js";/**
  * @license lucide-react v0.469.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- public/app/assets/rotate-ccw-nINu6_37.js
+++ public/app/assets/rotate-ccw-nINu6_37.js
@@ -1,4 +1,4 @@
-import{c as t}from"./index-DoIcKOYP.js";/**
+import{c as t}from"./index-C1UUmAtm.js";/**
  * @license lucide-react v0.469.0 - ISC
  *
  * This source code is licensed under the ISC license.
